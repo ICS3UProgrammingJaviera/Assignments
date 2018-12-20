4c42441089e043f664d991d2800c934e9dc8507c
17JavieraDiaz <43445003+17JavieraDiaz@users.noreply.github.com>
2018-12-20 10:38:26 -0500

    Initial commit

---
+++ FallingObjectsJaviera/FallingObjectsJaviera/FallingObjectsForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FallingObjectsJaviera
+{
+    partial class frmFallingObjectJaviera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterTime
+            // 
+            this.lblEnterTime.AutoSize = true;
+            this.lblEnterTime.Font = new System.Drawing.Font("Microsoft Yi Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterTime.Location = new System.Drawing.Point(83, 115);
+            this.lblEnterTime.Name = "lblEnterTime";
+            this.lblEnterTime.Size = new System.Drawing.Size(436, 21);
+            this.lblEnterTime.TabIndex = 0;
+            this.lblEnterTime.Text = "Enter time (in seconds) since you released the object";
+            // 
+            // frmFallingObjectJaviera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FallingObjectsJaviera.Properties.Resources.GravityBCimage2;
+            this.ClientSize = new System.Drawing.Size(1049, 667);
+            this.Controls.Add(this.lblEnterTime);
+            this.Name = "frmFallingObjectJaviera";
+            this.Text = "Falling Objects Assignment #2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterTime;
+    }
+}
+
